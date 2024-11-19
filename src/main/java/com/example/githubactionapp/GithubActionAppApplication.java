04598fdf99b96b2f13c0a2cbf89fcb1f5c34faed
lnformbu <lenonnformbui@gmail.com>
2024-11-19 01:40:30 -0500

    docs: add webapp docs - committed on 2024-11-19 01:40:29

---
+++ src/main/java/com/example/githubactionapp/GithubActionAppApplication.java
@@ -0,0 +1,21 @@
+package com.example.githubactionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActionAppApplication {
+
+    @GetMapping("/")
+    public String get(){
+        return "Welcome to lnformbu";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionAppApplication.class, args);
+    }
+
+}
